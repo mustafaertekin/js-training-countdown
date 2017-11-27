6d5def40f9a78e917878287ab7577bd972f36350
mustafaertekin <mustafa014@gmail.com>
2017-11-27 22:30:18 +0100

    some additions
    bootstrap form
    button added

--- js/main.js
+++ js/main.js
@@ -1,29 +1,37 @@
-let min = 2 - 1;  // 01:59 is the strat point for 2 minutes, so give -1 for the minute
 let sec = 60;
 
-let loop = setInterval(function() {
+$("#minButton").on("click", function () {
+    init();
+});
 
+function init() {
+    sec = 60;
+    let min = $("#minInput").val() - 1; // 01:59 is the strat point for 2 minutes, so give -1 for the minute
+    let loop = setInterval(function () {
+        sec--;
 
-    sec--;
-    
-    // if the sec = 0 , it means 1 minute is over, so min--
-    if (sec == 0){
-        sec = 60;
-        min --;
-    }
+        // if the sec = 0 , it means 1 minute is over, so min--
+        if (sec == 0) {
+            sec = 60;
+            min--;
+        }
 
-    // to make the view 01:05 instead of 1:5, we use padStart method with 2 digits parameter
-    let minute = new String(min).padStart(2, "0");    
-    let second = new String(sec).padStart(2, "0"); 
+        // to make the view 01:05 instead of 1:5, we use padStart method with 2 digits parameter
+        let minute = new String(min).padStart(2, "0");
+        let second = new String(sec).padStart(2, "0");
 
-    // Output to DOM with string literals
-    $("#main").html(`${minute} : ${second}`) 
+        // Output to DOM with string literals
+        $("#countdown").html(`${minute} : ${second}`)
 
-    // cancal the time interval when the min & sec 0
-    if (min == -1) {
-        // at the and write "the end"
-        clearInterval(loop);
-            $("#main").html("The End");
-    }
-}, 1000);
+        // cancal the time interval when the min & sec 0
+        if (min == -1) {
+            // at the and write "the end"
+            clearInterval(loop);
+            $("#countdown").html("The End");
+        }
 
+        $("#minButton").on("click", function () {
+            clearInterval(loop);
+        });
+    }, 1000);
+}
