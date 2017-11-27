32d0075b10bc76fee8f6818d0a05177e432b78b2
mustafaertekin <mustafa014@gmail.com>
2017-11-27 20:40:47 +0100

    second commit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
-let minToCountDown = 2;
-let min = 2;
+let min = 2 - 1;  // 01:59 is the strat point for 2 minutes, so give -1 for the minute
 let sec = 60;
 
 let loop = setInterval(function() {
 
+
     sec--;
     
+    // if the sec = 0 , it means 1 minute is over, so min--
     if (sec == 0){
         sec = 60;
         min --;
     }
 
-    $("#main").html(`${min-1}:${sec}`) 
+    // to make the view 01:05 instead of 1:5, we use padStart method with 2 digits parameter
+    let minute = new String(min).padStart(2, "0");    
+    let second = new String(sec).padStart(2, "0"); 
 
+    // Output to DOM with string literals
+    $("#main").html(`${minute} : ${second}`) 
+
+    // cancal the time interval when the min & sec 0
     if (min == 0 && sec == 0) {
         // at the and write "the end"
         if (minToCountDown < 0) {
